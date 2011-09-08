e4cbb80cd0f34d377a5e8b4fcdd1a9bc71f10e48
autoFilter <mooFilter@gmail.com>
2011-09-08 06:23:25 -0400

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'cc056e44b0dd636d3994d7b4889d9a307fb978955c07ebb2b48095ed9b45ab4c0f8e2426a50b7fdd4d365e342c19b9975107b6e609e5f8a8831c8b8a40547a20'
+FirstApp::Application.config.secret_token = '92c27e76ea953ba3438a94e8ed6a06f05b5ea3857f4c08bd580fd62858821a3ee80c56db784b626d39cb450028273990c9a6af661bfaa46e0a86f4318b205ca1'
